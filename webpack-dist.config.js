8a363863803cf3aa6ad599db6bb78fcc50fe13bd
Deoncn <2210633536@qq.com>
2021-09-09 13:55:22 +0800

    Update webpack-dist.config.js

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -59,7 +59,7 @@ module.exports = {
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://deoncn.github.io' // 这里注意改成你的在线简历的网站
+        'http://456d.top' // 这里注意改成你的在线简历的网站
       ]);
     }),
   ]
